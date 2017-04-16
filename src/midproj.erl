361220b77cf07a4492b03cb0f260e1c4a3621ee1
fixwin <rongalitayyonatan>
2017-04-16 17:53:24 +0300

    a

--- src/midproj.erl
+++ src/midproj.erl
@@ -15,30 +15,32 @@
 
 solve_bdd(BddTree, List)->
   Map = listToMap(List,#{}),
-  treeTraversal(BddTree,Map).
+  treeSearch(BddTree,Map).
 
-treeTraversal({true},Map)->true;
-treeTraversal({false},Map)->false;
-treeTraversal(Tree,Map) ->
+%Searches the tree according to the values given in map
+treeSearch({true},_Map)->true;
+treeSearch({false},_Map)->false;
+treeSearch(Tree,Map) ->
   CurrentAtom = element(1,Tree),
   AtomVal = maps:get(CurrentAtom,Map),
   LeftChild = element(2,Tree),
   RightChild = element(3,Tree),
   if
-    AtomVal -> treeTraversal(RightChild,Map);%if to go right
-    true -> treeTraversal(LeftChild,Map)
+    AtomVal -> treeSearch(RightChild,Map);%if to go right
+    true -> treeSearch(LeftChild,Map)
   end
 .
 
 exp_to_bdd(BoolFunc, Ordering)->
   %A = {'or', { {'or', { { 'and', { {'and', {{'not', x1},{'not', x2}} }, {'not', x3}} } , {'and',{x1,x2}} } } , {'and', {x2,x3}} }} ,
-  A = {'or',{ {'or',{ {'and',{ x1 , {'not', x2} }} , {'and',{ x2 , x3 }} }} , x3 }},
-  Vars = remove_dups(findVars([],A)),
+  %A = {'or',{ {'or',{ {'and',{ x1 , {'not', x2} }} , {'and',{ x2 , x3 }} }} , x3 }},
+  Vars = remove_dups(findVars([],BoolFunc)),
   VarPerms = perms(Vars),
   %io:fwrite(lists:flatten(io_lib:format("~p \n\n", [VarPerms]))),
-  OptimalTupel = getOptimal(A,VarPerms,Ordering,-1,{}),
+  OptimalTupel = getOptimal(BoolFunc,VarPerms,Ordering,-1,{}),
   element(1,OptimalTupel).
 
+%gets the optimal tree according to the Ordering Var
 getOptimal(_Exp,[],_Ordering,_CurrentMinOrdering,OptimalTupel)->OptimalTupel;
 getOptimal(Exp,Perms,Ordering,CurrentMinOrdering,OptimalTupel)->%on first permutation CurrentMinOrdering should be negative
   CurrentTupel = getTreeAndParams(Exp,hd(Perms)),
@@ -50,21 +52,22 @@ getOptimal(Exp,Perms,Ordering,CurrentMinOrdering,OptimalTupel)->%on first permut
     true -> getOptimal(Exp,tl(Perms),Ordering,CurrentMinOrdering,OptimalTupel)
   end.
 
+%returns a tuple with tree representation and parameter map
 getTreeAndParams(Exp, Vars) ->
   NewTree = buildInitialTree(Exp,#{},Vars),
   {NewTree,getTreeParams(NewTree,#{height=>0,numOfNodes=>0,numOfLeafs=>0},0)}.
 
-test() ->
-  %A = {'or',{ {'or',{ {'and',{ x1 , {'not', x2} }} , {'and',{ x2 , x3 }} }} , x3 }},
-  A = {'or', { {'or', { { 'and', { {'and', {{'not', x1},{'not', x2}} }, {'not', x3}} } , {'and',{x1,x2}} } } , {'and', {x2,x3}} }} ,
-  %A = {'and', {x1,{'not',x1}}},
-  Vars = remove_dups(findVars([],A)),
-  %Map = listToMap(Vars,#{}),Map,
-  Tree = buildInitialTree(A,#{},Vars),
-  Params = #{height=>0,numOfNodes=>0,numOfLeafs=>0},%height of only root is 0
-  getTreeParams(Tree,Params,0).
+%%test() ->
+%%  %A = {'or',{ {'or',{ {'and',{ x1 , {'not', x2} }} , {'and',{ x2 , x3 }} }} , x3 }},
+%%  A = {'or', { {'or', { { 'and', { {'and', {{'not', x1},{'not', x2}} }, {'not', x3}} } , {'and',{x1,x2}} } } , {'and', {x2,x3}} }} ,
+%%  %A = {'and', {x1,{'not',x1}}},
+%%  Vars = remove_dups(findVars([],A)),
+%%  %Map = listToMap(Vars,#{}),Map,
+%%  Tree = buildInitialTree(A,#{},Vars),
+%%  Params = #{height=>0,numOfNodes=>0,numOfLeafs=>0},%height of only root is 0
+%%  getTreeParams(Tree,Params,0).
 
-buildInitialTree(Exp, Map, [])->{solveExp(Exp,Map)};
+buildInitialTree(Exp, Map, [])->{solveExp(Exp,Map)}; %builds the tree and minimizes it
 buildInitialTree(Exp, Map, [H|T]) ->
   Left =  buildInitialTree(Exp, maps:put(element(1,H),false,Map),T),
   Right = buildInitialTree(Exp, maps:put(element(1,H),true,Map),T),
@@ -77,13 +80,14 @@ perms([]) -> [[]]; %finds all permutations of list
 perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
 
 %TODO change if x1,x2,x3 are not shown
-getTreeParams(Tree,Params,Depth) when Tree=:={true} -> getTreeParams({false},Params,Depth);
+%Traversal of the tree while updating Tree Parameters
+getTreeParams(Tree,Params,Depth) when Tree=:={true} -> getTreeParams({false},Params,Depth);%when reached a leaf
 getTreeParams(Tree,Params,Depth) when Tree=:={false} ->
   Inc1 = fun(V) -> V + 1 end,
   DepthFunc = fun(V) -> max(Depth,V) end,
   NewParams2 = funcOnMapElem(numOfLeafs,Inc1,Params),
   funcOnMapElem(height,DepthFunc,NewParams2);
-
+%when reached a node
 getTreeParams(Tree,Params,Depth)->
   Inc1 = fun(V) -> V + 1 end,
   NewParams = funcOnMapElem(numOfNodes,Inc1,Params),
@@ -91,18 +95,19 @@ getTreeParams(Tree,Params,Depth)->
   LeftParams = getTreeParams(Left,NewParams,Depth+1),
   getTreeParams(Right,LeftParams,Depth+1).
 
+%run a function on a specific element of a map
 funcOnMapElem(Key,Func,Map) ->
   OldVal = element(2,maps:find(Key,Map)),
   NewVal = Func(OldVal),
   maps:update(Key,NewVal,Map).
 
-solveExp({'or',{L,R}},VarMap)->solveExp(L,VarMap) or solveExp(R,VarMap);
+solveExp({'or',{L,R}},VarMap)->solveExp(L,VarMap) or solveExp(R,VarMap);%soelves the expression according to the values supplied in VarMap
 solveExp({'and',{L,R}},VarMap)->solveExp(L,VarMap) and solveExp(R,VarMap);
 solveExp({'not',R},VarMap)->not solveExp(R,VarMap);
 solveExp(Val,VarMap) -> maps:get(Val, VarMap).
 
 
-listToMap([],Map)->Map;
+listToMap([],Map)->Map;%generates a map with atoms representing boolean operators, and their values
 listToMap(List,Map)->listToMap(tl(List),maps:put(element(1,hd(List)), element(2,hd(List)), Map)).
 
 remove_dups([])    -> []; %remove duplicates in list
